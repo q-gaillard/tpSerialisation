6b11031932b274bd1e5c741aed95a9d25dc04beb
q-gaillard <quentingaillard@edouardgand.fr>
2026-03-31 11:52:36 +0200

    création fichier csv

--- Program.cs
+++ Program.cs
@@ -9,5 +9,7 @@ class Program
         gestionJeux.AjouterJeu(new JeuVideo("Mario Kart 8 Deluxe", "Nintendo", new DateTime(2017, 10, 20), 59.99));
         gestionJeux.AjouterJeu(new JeuVideo("The Witcher 3", "CD Projekt", new DateTime(2015, 5, 19), 39.99));
         gestionJeux.AfficherJeux();
+
+        GestionJeux gestionJeux2 = new GestionJeux();
     }
 }
